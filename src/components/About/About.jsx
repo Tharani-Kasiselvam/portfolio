15e53d91b33c385f745d428cfc5ca7b069f5150e
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2025-03-04 23:07:43 +0530

    fixing image loading issues

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,8 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src='./aboutImage.png'
+          // src='../aboutImage.png'
+          src = '../../aboutImage.png'
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
