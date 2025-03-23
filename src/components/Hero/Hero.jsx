51c15cd51b80b14977ba2ba22fa5a968b2587b6e
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2025-03-23 19:22:50 +0530

    updated About info

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,21 +9,18 @@ export const Hero = () => {
         <h2 className={styles.title}>Hi, I'm Tharani</h2>
         <p className={styles.description}>
         Results-driven Technical Manager, specializing in Incident Management
-        and Problem resolution. Demonstrated success in leading process
+        and Problem resolution. <br /><br />
+        Demonstrated success in leading process
         enhancements that significantly improved customer satisfaction and
         operational efficiency. 
         <br /><br />
         Skilled in mentoring teams and delivering technical
         guidance, effectively driving solutions in fast-paced environments.
           </p>
-          
-          <br />
-        <p className={styles.description}>
-          I'm a Full-Stack Developer with 1 year of experience using React and NodeJS. </p>
-          
+                   
           <br/>
           <p>
-          Reach out if you'd like to learn more!
+          <h3>Reach out if you'd like to learn more!</h3>
         </p>
         <br />
         <a href="mailto:sherley.tharani@gmail.com" className={styles.contactBtn}>
